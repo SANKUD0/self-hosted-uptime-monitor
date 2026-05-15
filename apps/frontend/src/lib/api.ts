5e64b1856b51ed78ef4c82d484520070dcc21dd9
SANKUD0 <tacofauche@gmail.com>
2026-05-15 12:31:42 -0400

    feat: add incident fetching methods and response types to API

--- apps/frontend/src/lib/api.ts
+++ apps/frontend/src/lib/api.ts
@@ -1,3 +1,5 @@
+import { get } from "http";
+
 const BASE_URL = process.env.MONOLITH_API_URL || "http://localhost:3001";
 
 export type NumberServiceResponse = {
@@ -8,6 +10,9 @@ export type UpServiceResponse = {
 }
 export type DownServiceResponse = {
     DownServices: number;
+}
+export type CountIncidentsResponse = {
+    count: number;
 }
 
 
@@ -29,6 +34,22 @@ export const api = {
         getCountDown: () => fetch(`${BASE_URL}/services/count/down`).then(res => {
             if (!res.ok) throw new Error(`HTTP ${res.status} `);
             return res.json() as Promise<DownServiceResponse>
+        }),
+    },
+    incidents: {
+        getAll: () => fetch(`${BASE_URL}/incidents`).then(res => {
+            if (!res.ok) throw new Error(`HTTP ${res.status} `);
+            return res.json()
+        }),
+        getCount: () => fetch(`${BASE_URL}/incidents/count/open`).then(res => {
+            if (!res.ok) throw new Error(`HTTP ${res.status} `);
+            return res.json() as Promise<CountIncidentsResponse>
+        }),
+    },
+    monitoring: {
+        getAll: () => fetch(`${BASE_URL}/monitoring`).then(res => {
+            if (!res.ok) throw new Error(`HTTP ${res.status} `);
+            return res.json()
         }),
     }
 }
