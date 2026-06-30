78c23f0f7cbf1384d54465d13e4d4fc1a2f02a86
SANKUD0 <tacofauche@gmail.com>
2026-06-30 18:25:41 -0400

    feat: remove 'enabled' column from NotificationChannelConfig model

---
+++ apps/backend/prisma/migrations/20260630211937_notification_configuration_change/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `enabled` on the `NotificationChannelConfig` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "NotificationChannelConfig" DROP COLUMN "enabled";
